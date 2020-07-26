81f1995aaa2d438aa26ebd88ee1ff0929287124c
zoverlvx <zoverhulser@gmail.com>
2020-07-26 14:48:45 -0400

    Abstracts out different data sets based upon need.

---
+++ client/src/data/definitions.js
@@ -0,0 +1,251 @@
+export default [
+		{	number: "1",
+			text: [ 
+				{
+					word: "In", 
+					translation: "in",
+					partOfSpeech: "preposition"
+				},
+				{
+					word: "St. Jago",
+					original: "das St. Jago", 
+					translation: "Santiago",
+					partOfSpeech: "proper noun"
+				},
+				{
+					word: "der", 
+					translation: "the",
+					partOfSpeech: "definite article",
+				},
+				{
+					word: "Hauptstadt", 
+					original: "die Hauptstadt",
+					translation: "capital",
+					partOfSpeech: "noun"
+				},
+				{
+					word: "des",
+					translation: "of the",
+					partOfSpeech: "definite article",
+				},
+				{
+					word: "Königreichs", 
+					original: "das Königreich",
+					translation: "kingdom",
+					partOfSpeech: "noun",
+				},
+				{
+					word: "Chili,", 
+					original: "das Chili",
+					translation: "Chile",
+					partOfSpeech: "proper noun",
+				},
+				{
+					word: "stand", 
+					partOfSpeech: "past tense of stehen",
+					translation: "stood"
+				},
+				{
+					word: "gerade",
+					partOfSpeech: "adverb"
+				},
+				{
+					word: "in",
+					translation: "in", 
+					partOfSpeech: "preposition"
+				},
+				{
+					word: "dem",
+					translation: "the",
+					partOfSpeech: "definite article",
+				},
+				{
+					word: "Augenblicke", 
+					original: "der Augenblick",
+					translation: "blink of an eye, moment",
+					partOfSpeech: "noun"
+				},
+				{
+					word: "der",
+					translation: "of the",
+					partOfSpeech: "definite article",
+				},
+				{
+					word: "grossen", 
+					original: "gross",
+					translation: "great",
+					partOfSpeech: "adjective",
+				},
+				{
+					word: "Erderschütterung", 
+					original: "die Erderschütterung",
+					translation: "earthquake",
+					partOfSpeech: "noun"
+				},
+				{
+					word: "vom",
+					original: "von dem",
+					translation: "from the",
+					partOfSpeech: "preposition and article contraction",
+					context: "This is the contraction of the preposition 'von' with the article 'dem'"
+					
+				},
+				{
+					word: "Jahre", 
+					original: "das Jahr",
+					translation: "year",
+					partOfSpeech: "noun",
+				},
+				{
+					word: "1647,", 
+					original: "1647",
+					partOfSpeech: "noun",
+				},
+				{
+					word: "bei",
+					translation: "by", 
+					partOfSpeech: "preposition"
+				},
+				{
+					word: "welcher", 
+					original: "welch",
+					translation: "which",
+					partOfSpeech: "pronoun",
+				},
+				{
+					word: "viele", 
+					translation: "many",
+					partOfSpeech: "adjective"
+				},
+				{
+					word: "tausend",
+					translation: "thousand",
+					partOfSpeech: "adjective"
+				},
+				{
+					word: "Menschen", 
+					original: "sing: der Mensch, pl: die Menschen",
+					translation: "people",
+					partOfSpeech: "noun"
+				},
+				{
+					word: "ihren", 
+					original: "ihr",
+					translation: "their",
+					partOfSpeech: "possessive adjective"
+				},
+				{
+					word: "Untergang", 
+					original: "der Untergang",
+					translation: "doom",
+					partOfSpeech: "noun"
+				},
+				{
+					word: "fanden,",
+					translation: "found", 
+					partOfSpeech: "past tense of finden"
+				},
+				{
+					word: "ein"
+					translation: "a",
+					partOfSpeech: "indefinite article"
+				},
+				{
+					word: "junger,", 
+					original: "jung",
+					translation: "young",
+					partOfSpeech: "adjective"
+				},
+				{
+					word: "auf",
+					translation: "on",
+					partOfSpeech: "preposition"
+				},
+				{
+					word: "ein"
+				},
+				{
+					word: "Verbrechen", 
+					original: "das Verbrechen"
+				},
+				{
+					word: "angeklagter", 
+					original: "angeklagt"
+				},
+				{
+					word: "Spanier"
+				},
+				{word: "namens"},
+				{word: "Jeronimo Rugera"},
+				{word: "an", pos: "preposition"},
+				{word: "einem", original: "ein"},
+				{word: "Pfeiler", original: "der Pfeiler"},
+				{word: "des"},
+				{word: "Gefängnisses,", original: "das Gefängnis"},
+				{word: "in", pos: "preposition"},
+				{word: "welches", original: "welch"},
+				{word: "man"},
+				{word: "ihn"},
+				{word: "eingesperrt", original: "einsperren"},
+				{word: "hatte,", original: "haben"},
+				{word: "und"},
+				{word: "wollte", original: "wollen"},
+				{word: "sich"},
+				{word: "erhenken.", original: "erhenken"},
+				{word: "Don Henrico Asteron,", original: "Don Henrico Asteron"},
+				{word: "einer", original: "ein"},
+				{word: "der"},
+				{word: "reichsten", original: "reich"},
+				{word: "Edelleute", original: "die Edelleute"},
+				{word: "der"},
+				{word: "Stadt,", original: "die Stadt"},
+				{word: "hatte", original: "haben"},
+				{word: "ihn"},
+				{word: "ohngefähr"},
+				{word: "ein"},
+				{word: "Jahr", original: "das Jahr"},
+				{word: "zuvor"},
+				{word: "aus", pos: "dative preposition"},
+				{word: "Hause,", original: "das Haus"},
+				{word: "wo"},
+				{word: "er", pos: "third person singular pronoun"},
+				{word: "als"},
+				{word: "Lehrer", original: "der Lehrer"},
+				{word: "angestellt", original: "anstellen"},
+				{word: "war,", original: "sein"},
+				{word: "entfernt,", original: "entfernen"},
+				{word: "weil"},
+				{word: "er"},
+				{word: "sich"},
+				{word: "mit", pos: "dative preposition"},
+				{word: "Donna Joesphe,", original: "Donna Josephe"},
+				{word: "seiner"},
+				{word: "einzigen", original: "einzig"},
+				{word: "Tochter,", original: "die Tochter"},
+				{word: "in"},
+				{word: "einem"},
+				{word: "zärtlichen", original: "zärtlich"},
+				{word: "Einverständnis", original: "das Einverständnis"},
+				{word: "befunden", original: "befinden"},
+				{word: "hatte.", original: "haben"}
+            ]
+		},
+
+		{
+			number: "2",
+			text: [
+				{word: "Die"}, 
+				{word: "zweite"}, 
+				{word: "Seite.", original: "die Seite"}
+			]
+		},
+		
+		{
+			number: "3",
+			text: [
+				{word: "Die"},
+				{word: "dritte"},
+				{word: "Seite.", original: "die Seite"}
+			]
+		},
+];
