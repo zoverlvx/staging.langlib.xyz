2a2ec4a7caafecb8642a7a1092f5e153e72f9928
zoverlvx <zoverhulser@gmail.com>
2020-08-15 16:23:58 -0400

    Adds style to individual buttons and their container

--- client/src/components/LanguageButton.js
+++ client/src/components/LanguageButton.js
@@ -5,9 +5,21 @@ import useHover from "../utils/useHover.js";
 export default function(props) {
 	const [hoverRef, isHovered] = useHover();
 	const languagePath = props.language.name.toLowerCase();
+	const style = {
+		display: "flex",
+		textDecoration: "none",
+		background: "#999",
+		padding: "1em 2.4em",
+		fontSize: ".9em",
+		margin: "1em",
+		color: "white",
+		flexGrow: "1",
+		textAlign: "center",
+	};
 	return (
 		<div ref={hoverRef}>
 			<NavLink 
+				style={style}
 				to={`/${languagePath}`}
 			>
 				{
--- client/src/components/LanguagesContainer.js
+++ client/src/components/LanguagesContainer.js
@@ -2,8 +2,16 @@ import React from "react";
 import LanguageButtons from "./LanguageButtons.js";
 
 export default function(props) {
+	const style = {
+		margin: "10px auto",
+		display: "flex",
+		flexDirection: "row",
+		justifyContent: "space-between",
+		flexWrap: "wrap",
+		maxWidth: "320px"
+	};
 	return (
-		<div>
+		<div style={style}>
 			<LanguageButtons languages={props.languages} />
 		</div>
 	);
